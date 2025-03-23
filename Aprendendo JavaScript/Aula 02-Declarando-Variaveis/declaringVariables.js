99e61a307855d680adcb10f0b874261be073f056
viiihfelix <vik.felix20@gmail.com>
2025-03-22 21:16:21 -0300

    Adicionada pasta Aula 02-Declarando-Variaveis

---
+++ Aprendendo JavaScript/Aula 02-Declarando-Variaveis/declaringVariables.js
@@ -0,0 +1,33 @@
+// let const
+
+const primeiroValor = 10;
+const segundoValor = 20;
+
+console.log("primeiroValor + segundoValor: ", primeiroValor + segundoValor);
+const valorTotal = primeiroValor + segundoValor;
+console.log("valorTotal: ", valorTotal);
+
+let terceiroValor = 50;
+console.log("terceitoValor: ", terceiroValor);
+
+terceiroValor = 100;
+console.log("terceiroValor final: ", terceiroValor);
+
+let comunidade = "Dev Completo"
+console.log('comunidade: ', comunidade);
+// const comunidade = 'Dev Completo'
+// const comundade = `Dev Completo`
+
+comunidade = 'CDC';
+console.log(`Comunidade: ${comunidade}`);
+
+// padrão camelCase para declarar variáveis
+const autoraDoCurso = "Beatriz Schwartz";
+// padrão snake_case
+const autora_do_curso = "Beatriz Schwartz";
+
+// const $palavras = '';
+// const _palavras = '';
+
+let x = 10;
+x = 'dez';
